93cd7f324ea36071e8d483237003286211cc5e09
Manuel Casanova <manucasanova@hotmail.com>
2022-04-18 05:48:49 -0700

    removed not used functions, reorganized

--- vampire.js
+++ vampire.js
@@ -48,61 +48,6 @@ class Vampire {
 
   /** Tree traversal methods **/
 
-  // Returns the vampire object with that name, or null if no vampire exists with that name
-  vampireWithName(name) {
-    
-  }
-
-  // Returns the total number of vampires that exist
-  get totalDescendents() {
-    
-  }
-
-  // Returns an array of all the vampires that were converted after 1980
-  get allMillennialVampires() {
-    
-  }
-
-  /** Stretch **/
-
-  // Returns the closest common ancestor of two vampires.
-  // The closest common anscestor should be the more senior vampire if a direct ancestor is used.
-  // For example:
-  // * when comparing Ansel and Sarah, Ansel is the closest common anscestor.
-  // * when comparing Ansel and Andrew, Ansel is the closest common anscestor.
-  closestCommonAncestor(vampire) {
-    if (this === vampire){
-      return this
-    }
-    let senior;
-    let junior;
-    if(this.isMoreSeniorThan(vampire)){
-      senior = this
-      junior = vampire
-    } else {
-      junior = this
-      senior = vampire
-    }
-    
-    if(senior.isDirectAncestor(junior)){
-      return senior
-    } else {
-      return senior.creator.closestCommonAncestor(junior)
-    }
-  }
-
-  isDirectAncestor(vampire) {
-    if(this.offspring.includes(vampire)) {
-      return true;
-    } else {
-      for (let v of this.offspring){
-        if(v.isDirectAncestor(vampire)){
-          return true
-        }
-      }
-    }
-    return false
-  }
 
 // Returns the vampire object with that name, or null if no vampire exists with that name
 vampireWithName(name) {
@@ -145,7 +90,46 @@ get allMillennialVampires() {
   return millenialVampire;
 }
 
+  /** Stretch **/
 
+  // Returns the closest common ancestor of two vampires.
+  // The closest common anscestor should be the more senior vampire if a direct ancestor is used.
+  // For example:
+  // * when comparing Ansel and Sarah, Ansel is the closest common anscestor.
+  // * when comparing Ansel and Andrew, Ansel is the closest common anscestor.
+  closestCommonAncestor(vampire) {
+    if (this === vampire){
+      return this
+    }
+    let senior;
+    let junior;
+    if(this.isMoreSeniorThan(vampire)){
+      senior = this
+      junior = vampire
+    } else {
+      junior = this
+      senior = vampire
+    }
+    
+    if(senior.isDirectAncestor(junior)){
+      return senior
+    } else {
+      return senior.creator.closestCommonAncestor(junior)
+    }
+  }
+
+  isDirectAncestor(vampire) {
+    if(this.offspring.includes(vampire)) {
+      return true;
+    } else {
+      for (let v of this.offspring){
+        if(v.isDirectAncestor(vampire)){
+          return true
+        }
+      }
+    }
+    return false
+  }
 
 }
 
